f9f789a7f68b19ec9391c7ffbe31420c63fb1da2
Zenobius Jiricek <zenobius.jiricek@gmail.com>
2013-08-26 18:33:36 +0930

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+import os
+
+setup(
+    name='django-factory-boy-command',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Zenobius Jiricek',
+    author_email='airtonix@gmail.com',
+    description='Simple django management command for factory-boy',
+    license='MIT',
+    keywords='django, fixtures, mockups, factory-boy',
+    url='https://github.com/airtonix/factory-boy-command',
+    include_package_data=True,
+)
+
