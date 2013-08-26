c2a692a99db29bcae4ffcfa7174bbf88dd031176
Zenobius Jiricek <zenobius.jiricek@gmail.com>
2013-08-26 19:19:41 +0930

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,10 @@ setup(
     name='django-factory-boy-command',
     version='0.0.1',
     packages=find_packages(),
+    install_requires=[
+        'factory-boy>=2.1.2',
+        'Django>=1.4'
+    ],
     author='Zenobius Jiricek',
     author_email='airtonix@gmail.com',
     description='Simple django management command for factory-boy',
